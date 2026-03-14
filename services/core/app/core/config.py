fbf8780c82f65218ab2c7a588e3b3204b73cbff3
Saschax99 <dol.sascha@googlemail.com>
2026-03-14 20:34:18 +0100

    fixed .env selection in correct folder

--- services/core/app/core/config.py
+++ services/core/app/core/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from pydantic import field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-env_path = Path(__file__).parent.parent.parent.parent / ".env"
+env_path = Path(__file__).parent.parent.parent / ".env"
 if env_path.exists():
     load_dotenv(env_path)
 else:
